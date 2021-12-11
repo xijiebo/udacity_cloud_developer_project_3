ab9485aba2424dc0aa88460b32f0daca89653f28
Jiebo Xi <xijiebo@gmail.com>
2021-12-11 17:23:46 -0600

    updated front end env

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://af9c235b5d0a04f8d9bed427393d4266-196306615.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
